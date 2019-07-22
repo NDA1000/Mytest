85705ae9efd48ec462d228591b09d2965eefd40e
NDA1000 <nnoko.degrando@gmail.com>
2019-07-22 20:40:40 +0100

    this is my first commit

---
+++ sample.js
@@ -0,0 +1,21 @@
+<script>
+
+	console.log("the man ran away and left his wife");
+
+	// this is just a simple javascript function
+
+	function counter(startNumber){
+
+		for(var i = startNumber; i < 20; i++){
+	
+			var result = i;
+			//return result;
+
+			//console.log(result); 
+			}
+		}
+	console.log(counter(10));
+</script>
+
+
+	
